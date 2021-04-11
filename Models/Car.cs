952acc6468990e16eea8137e04343c354dbd045b
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-04-11 18:04:48 -0500

    DRY code by using {get; set;}
    Co-authored-by: Shanen Cross <shanencross@yahoo.com>

--- Models/Car.cs
+++ Models/Car.cs
@@ -3,39 +3,23 @@ using System;
 namespace Dealership.Models {
   public class Car
   {
-    private string _makeModel;
-    private int _price;
-    private int _miles;
-    
+   public string MakeModel { get; set;}
+    public int Price { get; set;}
+    public int Miles { get; set;}
+      
     public Car(string makeModel, int price, int miles)
     {
-      _makeModel = makeModel;
-      _price = price;
-      _miles = miles;
+      MakeModel = makeModel;
+      Price = price;
+      Miles = miles;
     }
     
     public static string MakeSound(string sound) {
       return "Our cars sound like " + sound;
     }
 
-    public string GetMakeModel()
-    {
-      return _makeModel;
-    }
-    public int GetPrice()
-    {
-      return _price;
-    }
-    public void SetPrice(int newPrice)
-    {
-      _price = newPrice;
-    }
-    public int GetMiles()
-    {
-      return _miles;
-    }
     public bool WorthBuying(int maxPrice) {
-      return (_price <= maxPrice);
+      return (Price <= maxPrice);
     }
   }
 }
--- Program.cs
+++ Program.cs
@@ -11,9 +11,12 @@ namespace Dealership {
       Car yugo = new Car("1980 Yugo Koral", 700, 56000);  
       Car ford = new Car("1988 Ford Country Squire", 1400,  239001);     
       Car amc = new Car("1976 AMC Pacer", 400, 198000);
-        
+      Console.WriteLine(yugo.MakeModel);
+      yugo.MakeModel = "Worst Car Ever";
+      Console.WriteLine(yugo.MakeModel);
+
       List<Car> Cars = new List<Car>() {volkswagen, yugo,ford,amc};
-      yugo.SetPrice(300); 
+      yugo.Price = 300; 
       Console.WriteLine("Enter maximum Price: ");
       string stringMaxPrice = Console.ReadLine();
       int maxPrice = int.Parse(stringMaxPrice);
@@ -31,9 +34,9 @@ namespace Dealership {
       foreach(Car automobile in CarsMatchingSearch)
       {
         Console.WriteLine("---------");
-        Console.WriteLine(automobile.GetMakeModel());
-        Console.WriteLine(automobile.GetMiles() + " miles");
-        Console.WriteLine("$" + automobile.GetPrice());
+        Console.WriteLine(automobile.MakeModel);
+        Console.WriteLine(automobile.Miles + " miles");
+        Console.WriteLine("$" + automobile.Price);
       }
     }
   }
